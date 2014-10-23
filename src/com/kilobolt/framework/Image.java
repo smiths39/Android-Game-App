1d8002a9078478ad49695f8b7e7baaaa35f4c0d0
Sean Smith <sean.smith39@mail.dcu.ie>
2014-10-23 17:46:00 +0100

    First commit

---
+++ src/com/kilobolt/framework/Image.java
@@ -0,0 +1,11 @@
+package com.kilobolt.framework;
+
+import com.kilobolt.framework.Graphics.ImageFormat;
+
+public interface Image {
+
+	public int getWidth();
+	public int getHeight();
+	public ImageFormat getFormat();
+	public void dispose();
+}
